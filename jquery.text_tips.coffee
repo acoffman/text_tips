ee11158cbea7d63a7fdbc8f15409123124ad94f6
Adam Coffman <coffman.adam@gmail.com>
2011-12-15 17:24:50 -0600

    cache the selector

--- jquery.text_tips.coffee
+++ jquery.text_tips.coffee
@@ -7,17 +7,18 @@ $.fn.text_tips = (options) ->
 
   settings = $.extend(defaults, options)
   this.each ->
-    value = $(this).attr(settings.attribute)
-    $(this).val(value)
-    $(this).addClass(settings.inputClass)
+    $this = $(this)
+    value = $this.attr(settings.attribute)
+    $this.val(value)
+    $this.addClass(settings.inputClass)
 
-    $(this).on 'blur', (e) ->
-      if !$(this).val()
-        $(this).val(value)
-        $(this).addClass(settings.inputClass)
+    $this.on 'blur', (e) ->
+      if !$this.val()
+        $this.val(value)
+        $this.addClass(settings.inputClass)
 
-    $(this).on 'click', (e) ->
-      if $(this).val() == value
-        $(this).val('')
-        $(this).removeClass(settings.inputClass)
+    $this.on 'click', (e) ->
+      if $this.val() == value
+        $this.val('')
+        $this.removeClass(settings.inputClass)
   this
--- jquery.text_tips.js
+++ jquery.text_tips.js
@@ -12,20 +12,21 @@
     };
     settings = $.extend(defaults, options);
     this.each(function() {
-      var value;
-      value = $(this).attr(settings.attribute);
-      $(this).val(value);
-      $(this).addClass(settings.inputClass);
-      $(this).on('blur', function(e) {
-        if (!$(this).val()) {
-          $(this).val(value);
-          return $(this).addClass(settings.inputClass);
+      var $this, value;
+      $this = $(this);
+      value = $this.attr(settings.attribute);
+      $this.val(value);
+      $this.addClass(settings.inputClass);
+      $this.on('blur', function(e) {
+        if (!$this.val()) {
+          $this.val(value);
+          return $this.addClass(settings.inputClass);
         }
       });
-      return $(this).on('click', function(e) {
-        if ($(this).val() === value) {
-          $(this).val('');
-          return $(this).removeClass(settings.inputClass);
+      return $this.on('click', function(e) {
+        if ($this.val() === value) {
+          $this.val('');
+          return $this.removeClass(settings.inputClass);
         }
       });
     });
